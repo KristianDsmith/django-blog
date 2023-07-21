43e61a29b60d62772162ccb48436fda380b3f62a
KristianDsmith <>
2023-07-21 16:06:45 +0000

    Add new link to allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-project-2023.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [
+    'django-blog-project-2023-3961638af6ee.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -81,12 +82,12 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
+# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
