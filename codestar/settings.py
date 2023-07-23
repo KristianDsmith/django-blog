517af07f13eaa848a98eaf5d108cd67ce718beee
KristianDsmith <>
2023-07-23 13:44:02 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = [
     '8000-kristiandsmith-django-bl-sbio8h5hka.us2.codeanyapp.com', 'localhost']
