a62947e707a789e42237d7a6abcbc2cbcdf4f609
KristianDsmith <>
2023-07-23 12:59:11 +0000

    Add like and unlike to post page

--- codestar/urls.py
+++ codestar/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from blog.views import PostLike 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
     path('accounts/', include('allauth.urls')),
+    path('like/<slug:slug>', PostLike.as_view(), name='post_like'),
 ]
